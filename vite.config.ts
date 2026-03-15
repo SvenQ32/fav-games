b724f1e56f3c62b5ba0087466b496976805689d3
SvenQ32 <142423468+SvenQ32@users.noreply.github.com>
2026-03-15 13:36:26 +0100

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/fav-games/',   // <-- required for GitHub Pages
+  plugins: [
+    // React and Tailwind plugins are required
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  assetsInclude: ['**/*.svg', '**/*.csv'],
+  build: {
+    outDir: 'dist',       // <-- make sure this matches the workflow
+  },
+})
