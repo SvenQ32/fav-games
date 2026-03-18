e5691fc121bbafb5f86222be3a17710daa79422f
Kamerplant <142423468+Kamerplant24@users.noreply.github.com>
2026-03-18 22:12:27 +0100

    Update description for Mi Games configuration

--- src/app/config.ts
+++ src/app/config.ts
@@ -30,7 +30,7 @@ export const pagesConfig: PageItem[] = [
     background: '#000000',
     profilePicture: '/fav-games/images/crab-rave.gif', // Default profile picture
     title: 'Mi Games',
-    description: 'mijn favo games',
+    description: 'mijnvbcxcxbv favo games',
   },
   {
     title: "Wii U",
